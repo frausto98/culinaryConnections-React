5af1cd9d201ba718520d9a93a1bb894a0b9377b4
frausto98 <frausto9298@yahoo.com>
2024-02-11 14:38:42 -0800

    intend to add comments and allow the recipe poster to include their own opinoin on their recipe difficulty

--- server/models/Recipe.js
+++ server/models/Recipe.js
@@ -17,7 +17,43 @@ const recipeSchema = new Schema({
     steps: {
         type: String,
         required: true,
-    }
+    },
+    recipeDifficulty: {
+        type: Number,
+        min: 1,
+        max: 10,
+        required: true,
+    },
+    recipeAuthor: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+        get: (timestamp) => dateFormat(timestamp),
+    },
+    comments: [
+        {
+            commentText: {
+                type: String,
+                required: true,
+                minlength: 1,
+                maxlength: 280,
+            },
+            commentAuthor: {
+                type: String,
+                required: true,
+            },
+            createdAt: {
+                type: Date,
+                default: Date.now,
+                get: (timestamp) => dateFormat(timestamp),
+            },
+        },
+    ],
+    
 });
 
 const Recipe = model('Recipe', recipeSchema)
