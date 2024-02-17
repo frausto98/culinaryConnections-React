5d77bb623007c7a98ab8270e12044997669f3cf5
frausto98 <frausto9298@yahoo.com>
2024-02-17 14:15:55 -0800

    added 'definitions' to the query part of the resolvers. beginning to set up the mutations portion. been thinking of added comments AS I GO, but may save it for the end overall

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,34 @@
+//we import the User and Recipe models that we schemed up in the /Models folder
 const { User, Recipe } = require('../models')
+// we also import the sign token, and well as the AuthErr from the /Utils/Auth folder
 const { signToken, AuthenticationError } = require('../utils/auth')
 
 const resolvers = {
+    Query: {
+        users: async () => {
+            return User.find().populate('recipes');
+        },
+        user: async (parent, {username}) => {
+            return User.findOne({username}).populate('recipes');
+        },
+        recipes: async (parent, {username}) => {
+            const params = username ? {username} : {};
+            return Recipe.find(params).sort({createdAt: -1});
+        },
+        recipe: async (parent, {recipeId}) => {
+            return Recipe.findOne({_id: recipeId});
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('recipes');
+            }
+            throw AuthenticationError
+        }
+    },
 
+    Mutation: {
+
+    }
 }
 
 module.exports = resolvers
