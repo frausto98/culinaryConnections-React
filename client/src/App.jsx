82f6d8ecc7f9b97933c3ca5649114b0f3f329efa
frausto98 <frausto9298@yahoo.com>
2024-02-22 14:49:40 -0800

    began setting up main.jsx for React's Router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -19,12 +15,24 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        +
         </button>
-        <p>
+        <p>count is {count}
+        <br></br>
+        
           Edit <code>src/App.jsx</code> and save to test HMR
+        <br></br>
+          this is me testing the src/App.jsx file
         </p>
+        <button onClick = {() => setCount((count) => count - 1)} > - </button>
       </div>
+
+      <div>
+        <button onClick = {() => setCount((count) => count = 0)} >
+          reset
+        </button>
+      </div>
+
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/Page1',
+      },
+      {
+        path: '/Page2',
+      },
+      {
+        path: '/Page3'
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
