b4661dd66076ddb35085b4ee83c3cf86f3feda9a
frausto98 <frausto9298@yahoo.com>
2024-03-01 14:25:55 -0800

    added the queries JS to the utils folder. can now start populating the webpage with database information

---
+++ client/src/utils/queries.js
@@ -0,0 +1,105 @@
+import {gql} from '@apollo/client'
+
+export const ME = gql`
+query me {
+    me {
+      _id
+      email
+      username
+      firstName
+      lastName
+      recipes {
+        _id
+        ingredientCount
+        ingredients
+        stepCount
+        steps
+        recipeName
+        recipeDescription
+        recipeDifficulty
+        recipeAuthor
+        createdAt
+      }
+    }
+  }
+`
+
+export const SINGLE_USER = gql`
+query user($username: String!) {
+    user(username: $username) {
+      _id
+      email
+      username
+      firstName
+      lastName
+      recipes {
+        _id
+        ingredientCount
+        ingredients
+        stepCount
+        steps
+        recipeName
+        recipeDescription
+        recipeDifficulty
+        recipeAuthor
+        createdAt
+      }
+    }
+  }
+`
+
+export const SINGLE_RECIPE = gql`
+query recipe($recipeId: ID!) {
+    recipe(recipeId: $recipeId) {
+      _id
+      recipeName
+      ingredientCount
+      ingredients
+      stepCount
+      steps
+      recipeDescription
+      recipeDifficulty
+      recipeAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+      likes {
+        _id
+        like
+        likedBy
+      }
+    }
+  }
+`
+
+export const RECIPES = gql`
+query recipe {
+    recipe {
+      _id
+      recipeName
+      ingredientCount
+      ingredients
+      stepCount
+      steps
+      recipeDescription
+      recipeDifficulty
+      recipeAuthor
+      createdAt
+      comments {
+        _id
+        commentText
+        commentAuthor
+        createdAt
+      }
+      likes {
+        _id
+        like
+        likedBy
+      }
+    }
+  }
+`
