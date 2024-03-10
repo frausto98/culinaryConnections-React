3404d72aba8091e394c41adb9b673c970be44808
frausto98 <frausto9298@yahoo.com>
2024-03-09 16:07:03 -0800

    going to attempt to make my own modal from scratch, not use reactjs

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,8 +12,8 @@ import '../styles/Home.css'
 
 const Home = () => {
 
-    // const [open, setOpen] = useState(false)
-    // const closeModal = () => setOpen(false)
+    const [open, setOpen] = useState(false)
+    const closeModal = () => setOpen(false)
 
     return (
         <div id="homePage">
@@ -39,34 +39,9 @@ const Home = () => {
                         {/* below is the reactjs-popup.
                         The entire overlay has its own set of classNames and Ids for each HTML Element */}
 
-                        <Popup className="modal" id="homeModal"
-                            trigger={
                                 <button className="modalTrigger" id="homeModalTrigger" >
                                     Form is Here
                                 </button>
-                            }
-                            modal
-                        >
-                            <div className="modalCard" id="homeModalCard">
-                                <div className="modalHeader" id="homeModalHeader">
-                                    <Popup trigger={
-                                        <button className='formTrigger'> Sign Up </button>
-                                    }>
-                                        <div className="modalBody" id="signUpBody" >
-                                            <p>uhhhh??</p>
-                                        </div>
-                                    </Popup>
-                                    <Popup trigger={
-                                        <button className='formTrigger'> Log In </button>
-                                    }>
-                                        <div className="modalBody" id="loginBody" >
-
-                                        </div>
-                                    </Popup>
-                                </div>
-
-                            </div>
-                        </Popup>
 
                         {/* --------------------- */}
 
