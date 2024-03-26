36277b17a84a6089d5fabebcae79ff36616a173c
frausto98 <frausto9298@yahoo.com>
2024-03-26 15:31:20 -0700

    added the barebones of what i would want my recipe to look like

--- client/src/pages/RecipeForm.jsx
+++ client/src/pages/RecipeForm.jsx
@@ -7,7 +7,30 @@ const RecipeForm = () => {
 
     return (
         <>
-            <span>Hello World!</span>
+            <div>
+                <h3>What did you cook up?</h3>
+            </div>
+            <div>
+                <form>
+                    <fieldset>
+                        <legend>Recipe Basics</legend>
+                        <label>
+                            recipe name:
+                            <input></input>
+                        </label>
+                        <br/>
+                        <label>
+                            recipe description:
+                            <input></input>
+                        </label>
+                        <br/>
+                        <label>
+                            recipe difficulty:
+                            <input type="number" max={10} min={0}></input>
+                        </label>
+                    </fieldset>
+                </form>
+            </div>
             <div>
                 <Link className="linkBtn" to='/home'>Back to Home</Link>
             </div>
