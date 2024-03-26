ec98c35b6b724c2f46fff627f785b9429f697970
frausto98 <frausto9298@yahoo.com>
2024-03-26 15:50:16 -0700

    added more information to the specific input areas. That's not all the input areas that will be around, but i did get side tracked with trying to style the textarea and disabled the resize option, because i thought it was ugly.

--- client/src/pages/RecipeForm.jsx
+++ client/src/pages/RecipeForm.jsx
@@ -3,6 +3,18 @@ import { useMutation } from '@apollo/client';
 
 import { ADD_RECIPE } from "../utils/mutations";
 
+const styles ={
+    input: {
+        height: "20px",
+        width: "300px",
+    },
+    textArea: {
+        resize: "none",
+        height: "100px",
+        width: "300px",
+    }
+}
+
 const RecipeForm = () => {
 
     return (
@@ -16,17 +28,39 @@ const RecipeForm = () => {
                         <legend>Recipe Basics</legend>
                         <label>
                             recipe name:
-                            <input></input>
+                            <input
+                            type="text"
+                            name="recipeName"
+                            id="recipeName"
+                            placeholder="Your recipe name is..."
+                            //value
+                            //onChange
+                            style={styles.input}
+                            required/>
                         </label>
                         <br/>
                         <label>
                             recipe description:
-                            <input></input>
+                            <textarea
+                            name="recipeDescription"
+                            id="recipeDescription"
+                            placeholder="Please describe your recipe..."
+                            //value
+                            //onChange
+                            style={styles.textArea}
+                            required>
+                            </textarea>
                         </label>
                         <br/>
                         <label>
                             recipe difficulty:
-                            <input type="number" max={10} min={0}></input>
+                            <input 
+                            type="number" max={10} min={0}
+                            name="recipeDifficulty"
+                            id="recipeDifficulty"
+                            //value
+                            //onChange
+                            required/>
                         </label>
                     </fieldset>
                 </form>
