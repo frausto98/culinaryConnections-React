0eadf787129817848ff3d3365b520dddb9c994f7
frausto98 <frausto9298@yahoo.com>
2024-04-13 15:11:31 -0700

    got data to load for user on Profile jsx

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,8 +1,23 @@
+import { Link, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
+import { SINGLE_USER } from "../utils/queries";
+
 const Profile = () => {
+
+    const {username} = useParams();
+    const { loading, data } = useQuery(SINGLE_USER, {
+        variables: {username: username},
+    })
+
+    const userProf = data?.user || {}
+    
+    if (loading) {
+        return <>Loading...</>
+    }
 
     return (
         <>
-        <h1> Hello World! </h1>
+        <h1> Welcome to {userProf.username}'s Profile! </h1>
         </>
     )
 }
