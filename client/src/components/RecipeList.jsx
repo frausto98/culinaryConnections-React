5cc66c76109d1228aff2240f6afa875f0005de70
frausto98 <frausto9298@yahoo.com>
2024-04-14 15:12:29 -0700

    on the profile jsx, i added a username prop that could contain query data of a username. that prop is on the recipeList component jsx. using that prop on the component, if the prop is empty, then it will display ther list with only half the recipe data, and the link to thye details. however, is the component is loaded with data in the prop, then the list will just load all the details. this is because the profile jsx queries the data, whereas if the component was loaded from the MainPage jsx, the prop would not contain query data.

--- client/src/components/RecipeList.jsx
+++ client/src/components/RecipeList.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 
 const styles = {
     recipeBox: {
@@ -10,49 +10,91 @@ const styles = {
         marginTop: "15px",
         marginBottom: "15px",
         paddingTop: "15px",
-        paddingBottom:"15px",
+        paddingBottom: "15px",
         borderRadius: "15px"
     },
     recipeHeader: {
         background: "grey",
-        
+
     }
 }
 
-const RecipeList = ({ recipes,  }) => {
+const RecipeList = ({ recipes, username }) => {
+
+    const currentPage = useLocation().pathname;
+
+
+    // i want the Link button to not be active when viewing component from the Profile element
+    // i use the username prop that may or may not contain a username directly from the query data
+
     if (!recipes.length) {
         return <span> No Recipes... </span>
     }
 
+    // if one is not 
+    if (!username) {
+        return (
+            <>
+                <div>
+                    {recipes &&
+                        recipes.map((recipe) => (
+                            <div key={recipe._id} className="recipe" style={styles.recipeBox}>
+
+                                <div className="recipeHeader" style={styles.recipeHeader}>
+                                    <h4>
+                                        {recipe.recipeAuthor} <br />
+                                        <span>
+                                            Cooked this post on {recipe.createdAt}
+                                        </span>
+                                    </h4>
+                                </div>
+                                <div>
+                                    <h5>{recipe.recipeName}</h5>
+                                    <p>{recipe.recipeDescription}</p>
+
+                                </div>
+                                <div>
+                                    <Link className="linkBtn" to={`/recipes/${recipe._id}`}>
+                                        CLick to Get more Recipe Details!
+                                    </Link>
+                                </div>
+                            </div>
+                        ))}
+                </div>
+            </>
+        );
+    }
+
     return (
-        <>
-            <div>
-                {recipes &&
-                    recipes.map((recipe) => (
-                        <div key={recipe._id} className="recipe" style={styles.recipeBox}>
-                            
-                            <div className="recipeHeader" style={styles.recipeHeader}>
-                                <h4>
-                                    {recipe.recipeAuthor} <br />
-                                    <span>
-                                        Cooked this post on {recipe.createdAt}
-                                    </span>
-                                </h4>
-                            </div>
-                            <div>
-                                <h5>{recipe.recipeName}</h5>
-                                <p>{recipe.recipeDescription}</p>
-                            </div>
-                            <div>
-                                <Link className="linkBtn" to={`/recipes/${recipe._id}`}>
-                                    CLick to Get more Recipe Details!
-                                </Link>
-                            </div>
+        <div>
+            {recipes &&
+                recipes.map((recipe) => (
+                    <div key={recipe._id} className="recipe" style={styles.recipeBox}>
+
+                        <div className="recipeHeader" style={styles.recipeHeader}>
+                            <h4>
+                                <span>
+                                    Cooked this post on {recipe.createdAt}
+                                </span>
+                            </h4>
                         </div>
-                    ))}
-            </div>
-        </>
-    );
+                        <div>
+                            <h5>{recipe.recipeName}</h5>
+                            <p>{recipe.recipeDescription}</p>
+                            <p>recipe difficulty: {recipe.difficulty}</p>
+                        </div>
+                        <div>
+                            <p>Number of Steps: {recipe.stepCount}</p>
+                            <p>{recipe.steps}</p>
+                        </div>
+                        <div>
+                            <p>Number of ingredients: {recipe.ingredientCount}</p>
+                            <p>{recipe.ingredients}</p>
+                        </div>
+                    </div>
+                ))}
+        </div>
+    )
 };
 
 export default RecipeList
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -42,6 +42,7 @@ const Profile = () => {
                 <div>
                     <RecipeList
                         recipes={userProf.recipes}
+                        username={userProf.username}
                     />
                 </div>
             </div>
